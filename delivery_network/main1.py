87055f48cd2b5d3a12845c5d4fb6334c2d8f7741
flava <florian.lava@ensae.fr>
2023-04-04 19:43:41 +0000

    ok

--- delivery_network/main1.py
+++ delivery_network/main1.py
@@ -1,15 +1,29 @@
+import sys 
+sys.path.append("delivery_network/")
+
 from graph import Graph, graph_from_file, time_estimation, union_find, new_minpower, new_minpower_aux, bfs, route_from_file, truck_from_file
 from graph import truck_affectation, knapsack
 import time
 from time import perf_counter
-
-import sys 
-sys.path.append("delivery_network/")
 
 
 G = graph_from_file("input/network.1.in")
 list_trucks = truck_from_file("tests/trucks.0.in")
 list_route = route_from_file("input/routes.1.in")
 
-print(knapsack(G, list_trucks, list_route))
 
+def new_time_estimation(n):
+    with open("input/routes." + str(n) + ".in","r") as file:
+        time_est = 0
+        a = int(file.readline()) # We save the amount of itineraries
+        g = graph_from_file("input/network." + str(n) + ".in")
+        g.kruskal
+        for i in range(10): # Average with 10 itineraries
+            node1,node2,p = map(int, file.readline().split())
+            t1 = time.perf_counter()
+            print(t1)
+            opti = new_minpower_aux(g, node1, node2)
+            t2 = time.perf_counter()
+            time_est += (t2-t1)
+
+new_time_estimation(4)
